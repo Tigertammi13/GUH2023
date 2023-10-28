288ba6984b360061ede3ceb324053d13992874e2
n59321jh <joshua.hines@student.manchester.ac.uk>
2023-10-28 21:27:27 +0100

    Doesn't work yet lol

---
+++ GUH/Assets/Scripts/TransactionObject.cs
@@ -0,0 +1,9 @@
+﻿internal class TransactionObject
+{
+    public float amount;
+    public string transactionUUID;
+    public string merchantUUID;
+    public string countryUUID;
+    public string creditDebitIndicator;
+    public string currency;
+}
