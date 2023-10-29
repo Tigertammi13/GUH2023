ae6e32a8b4e0a1079730d8aee1b4879053f07249
Alex <dolodot13@gmail.com>
2023-10-29 01:12:19 +0100

    Add challenges in the repository

--- GUH/Assets/Scripts/Repository.cs
+++ GUH/Assets/Scripts/Repository.cs
@@ -8,13 +8,17 @@ public static class Repository
 {
     public static List<Transaction> transactions;
     public static List<Account > accounts;
+    public static List<Transaction> challanges;
     public static void loadData()
     {
         string transactionText = File.ReadAllText("transactions.json");
         string accountText= File.ReadAllText("accounts.json");
+        string challengeText = File.ReadAllText("transactions.json");
 
         transactions=JsonUtility.FromJson<List<Transaction>>(transactionText);
         accounts=JsonUtility.FromJson<List<Account>>(accountText);
+        challanges=JsonUtility.FromJson<List<Transaction>>(challengeText);z
+
 
     }
 }
